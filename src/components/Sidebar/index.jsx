4ff6f5c9bbe0466bba7ea3f6dcf8b6a200c9f883
Majd Ajroudi <majd.ajroudi@gmail.com>
2021-04-08 22:22:57 +0300

    image upload btn added

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React,{useState} from "react"
 import {Layout, Menu, Avatar} from "antd"
 import { UserOutlined } from '@ant-design/icons';
 import "./index.css"
@@ -7,15 +7,24 @@ function Sidebar(props)  {
   const {Sider} = Layout;
   const {SubMenu} = Menu;
 
+  // const handleFileSelect = (e) => {
+  //   const reader = new FileReader()
+  //   console.log(e.target.files[0])
+  //   reader.readAsDataURL(e.target.files[0])
+  //   reader.onload = function(e) {
+  //     setImageSrc(e.target.result)
+  //     console.log(e.target.result)
+  //   }
+  // }
+
   return(
       <Sider className="sidebar__bar">
         <div className="sidebar__bar__user">
-          <Avatar className="sidebar__bar__user__avatar" size={64} icon ={<UserOutlined />} />
+          <Avatar className="sidebar__bar__user__avatar" size={64} icon ={<UserOutlined />} alt="user-avatar" />
           <span className="sidebar__bar__user__name">{props.userName}</span>
         </div>
-        <Menu>
-          
-        </Menu>
+        <div className="siderbar__bar__menu">
+        </div>
       </Sider>
   )
 }
