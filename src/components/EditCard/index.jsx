1e7a65bdc43333f01004d0916711bfac188142a5
Majd Ajroudi <majd.ajroudi@gmail.com>
2021-04-09 12:11:55 +0300

    add handle edit on click

--- src/components/EditCard/index.jsx
+++ src/components/EditCard/index.jsx
@@ -1,9 +1,10 @@
-import React,{useState} from "react"
+import React,{useState, useRef, useEffect} from "react"
 import SingleItemField from "../SingleItemField"
 import {Modal, Button, Input} from "antd"
 
 function EditCard(props) {
     const [editTitleValue, setEditTitleValue] = useState(props.card.title)
+    const titleInputRef = useRef()
     
     const handleOk = (e) => {
         e.preventDefault()
@@ -21,31 +22,46 @@ function EditCard(props) {
         props.setIsModalVisible(false)
     }
 
+    const handleEnterClick = (e) => {
+        if (e.key === "Enter") {
+            handleOk(e)
+        }
+    }
+
+    useEffect(() => {
+        if (props.isModalVisible){
+            titleInputRef.current.focus()
+        }
+    }, [props.isModalVisible])
+
 
     return(
         <Modal visible={props.isModalVisible} onOk={(e) => handleOk(e)} onCancel={(e) => handleCancel(e)}>
-            <div className="modal-body-top">
-                <Input 
-                  placeholder="Edit Project's name"
-                  value={editTitleValue}
-                  onChange={(e) => setEditTitleValue(e.target.value)}
-                  />
-            </div>
-            <div className="modal-body-bottom">
-                <ul className="itemList">
-                    {props.card.items.map((currentItem, itemIndex) => {
-                        return(
-                            <SingleItemField 
-                            key={`item-${itemIndex}`} 
-                            itemName={currentItem}
-                            itemIndex={itemIndex} 
-                            cards={props.cards}
-                            currentCard={props.card}
-                            cardsFunction={props.cardsFunction}
-                            className="itemList__singleItem" />
-                        )
-                    })}
-                </ul>
+            <div onKeyDown={(e) => handleEnterClick(e)} className="modal-body">
+                <div className="modal-body-top">
+                    <Input 
+                    placeholder="Edit Project's name"
+                    value={editTitleValue}
+                    onChange={(e) => setEditTitleValue(e.target.value)}
+                    ref={titleInputRef}
+                    />
+                </div>
+                <div className="modal-body-bottom">
+                    <ul className="itemList">
+                        {props.card.items.map((currentItem, itemIndex) => {
+                            return(
+                                <SingleItemField 
+                                key={`item-${itemIndex}`} 
+                                itemName={currentItem}
+                                itemIndex={itemIndex} 
+                                cards={props.cards}
+                                currentCard={props.card}
+                                cardsFunction={props.cardsFunction}
+                                className="itemList__singleItem" />
+                            )
+                        })}
+                    </ul>
+                </div>
             </div>
         </Modal>
     )
